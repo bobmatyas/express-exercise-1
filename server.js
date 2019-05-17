91d5ea3f532cebbcb4199f8467dda851822c7dd3
bobmatyas <ballium@protonmail.com>
2019-05-17 12:21:58 -0500

    connected angular and got public directory setup

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 // require the Express module
 const express = require("express");
+
 // creates an instance of an Express server
 const app = express();
 
-//import cartItems route
+// server.js, any file with a created and configured Express app
+app.use(express.static('./public'));
+
+// //import cartItems route
 const items = require('./cartItems');
+
 //convert params and body into usable JSON
 app.use(express.json());
 
 
-// use and setup items route
+// // use and setup items route
 app.use("/", items);
 
 
